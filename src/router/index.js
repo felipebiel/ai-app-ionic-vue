b84161bd37bafec30537803974a8ff0a1e7df6ed
Felipe Biel <felipe.biel.souza@gmail.com>
2021-01-14 13:54:03 -0300

    new: Criação da view de Login e redirecionamento inicial para ela

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
 
 const routes = [
   {
     path: '/',
-    redirect: '/home'
+    redirect: '/login'
   },
   {
     path: '/home',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
   }
 ]
 
