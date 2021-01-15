bd1e6ff4224fc71255ffe9d57c57a705751fdfb2
Felipe Biel <felipe.biel.souza@gmail.com>
2021-01-15 10:01:55 -0300

    new: Validando o campos antes de enviar os dados do form para o pai em LoginForm

--- src/components/login/LoginForm.vue
+++ src/components/login/LoginForm.vue
@@ -2,29 +2,43 @@
   <ion-col size="12" class="">
     <ion-item class="ion-margin-top">
       <ion-icon :icon="person" class="position-absolute"></ion-icon>
-      <ion-input placeholder="Usuário" class="ion-text-center"></ion-input>
+      <ion-input
+        placeholder="Usuário"
+        class="ion-text-center"
+        v-model="form.username"
+      ></ion-input>
     </ion-item>
-    <ion-text color="danger" v-if="true">
+    <ion-text color="danger" v-if="usernameErrors">
       <p class="ion-no-margin ion-margin-top ion-text-center">
-        Esse campo é obrigatório
+        {{ usernameErrors }}
       </p>
     </ion-text>
   </ion-col>
   <ion-col size="12">
     <ion-item class="ion-margin-top">
       <ion-icon :icon="lockClosed" class="position-absolute"></ion-icon>
-      <ion-input placeholder="Senha" class="ion-text-center"></ion-input>
+      <ion-input
+        placeholder="Senha"
+        class="ion-text-center"
+        type="password"
+        v-model="form.password"
+      ></ion-input>
     </ion-item>
-    <ion-text color="danger" v-if="true">
+    <ion-text color="danger" v-if="passwordErros">
       <p class="ion-no-margin ion-margin-top ion-text-center">
-        Esse campo é obrigatório
+        {{ passwordErros }}
       </p>
     </ion-text>
   </ion-col>
   <ion-col size="12" class="ion-text-center">
-    <ion-button expand="block" color="secondary" class="ion-margin-top"
-      >Login</ion-button
+    <ion-button
+      expand="block"
+      color="secondary"
+      class="ion-margin-top"
+      @click="validatorLoginField"
     >
+      Login
+    </ion-button>
     <div class="ion-margin-top" v-if="true">
       <div margin class="white ion-text-center">Carregando</div>
       <ion-progress-bar
@@ -61,7 +75,41 @@ export default {
     return {
       person,
       lockClosed,
+      form: {
+        username: "",
+        password: "",
+      },
+      usernameErrors: "",
+      passwordErros: "",
     };
+  },
+  methods: {
+    validatorLoginField() {
+      const username = this.form.username;
+      const password = this.form.password;
+      this.usernameErrors = "";
+      this.passwordErros = "";
+      let erros = false;
+      if (username.trim() === "") {
+        this.usernameErrors = "Este campo é obrigatório";
+        erros = true;
+      }
+      if (password.length < 6) {
+        if (password.trim() === "") {
+          this.passwordErros = "Este campo é obrigatório";
+          erros = true;
+        } else {
+          this.passwordErros = "Este campo deve ter no mínimo 6 caracteres";
+          erros = true;
+        }
+      }
+      if (!erros) {
+        this.sendFormLogin();
+      }
+    },
+    sendFormLogin() {
+      console.log("Enviando o form");
+    },
   },
 };
 </script>
