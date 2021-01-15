ddf3b671e0e4f2280b84ebf6b7367d704bb52fc4
Felipe Biel <felipe.biel.souza@gmail.com>
2021-01-15 17:28:39 -0300

    new: Componente de base layout já usando as tabs

---
+++ src/components/base/BaseLayout.vue
@@ -0,0 +1,60 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-back-button
+            :default-href="pageDefaultBackLink"
+          ></ion-back-button>
+        </ion-buttons>
+        <ion-title> {{ pageTitle }}</ion-title>
+        <ion-buttons slot="end">
+          <!-- AQUI VAI VIR O MENU -->
+          <slot name="actions-end"></slot>
+        </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content>
+      <slot></slot>
+    </ion-content>
+
+    <!-- AQUI PUXAR TABS -->
+    <tabs></tabs>
+  </ion-page>
+</template>
+
+<script>
+import Tabs from "@/components/base/Tabs";
+
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonBackButton,
+  IonButtons,
+} from "@ionic/vue";
+export default {
+  name: "BaseLayout",
+  props: ["pageTitle", "pageDefaultBackLink"],
+  components: {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonBackButton,
+    IonButtons,
+    Tabs,
+  },
+};
+</script>
+
+<style scoped>
+ion-toolbar {
+  --background: var(--ion-color-primary);
+  --color: var(--ion-color-primary-contrast);
+}
+</style>
