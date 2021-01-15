dd25abe07e37bc15d13ba94837220077e80a2960
Felipe Biel <felipe.biel.souza@gmail.com>
2021-01-15 09:14:58 -0300

    new: Componentização do VerticalAlign

---
+++ src/components/base/VerticalAlign.vue
@@ -0,0 +1,29 @@
+<template>
+  <ion-content>
+    <ion-grid>
+      <ion-row
+        style="height: 98vh"
+        class="ion-align-items-center ion-justify-content-center"
+      >
+        <ion-col size="12">
+          <slot></slot>
+        </ion-col>
+      </ion-row>
+    </ion-grid>
+  </ion-content>
+</template>
+
+<script>
+import { IonContent, IonGrid, IonRow, IonCol } from "@ionic/vue";
+export default {
+  components: {
+    IonContent,
+    IonGrid,
+    IonRow,
+    IonCol,
+  },
+};
+</script>
+
+<style lang="css" scoped>
+</style>
