6ba6670342ce8b0b5cdc0132ad3c1d3dfb65ee5a
Felipe Biel <felipe.biel.souza@gmail.com>
2021-01-20 15:38:41 -0300

    new: status da bomba de forma estática

--- src/pages/Status.vue
+++ src/pages/Status.vue
@@ -1,44 +1,66 @@
 <template>
-  <base-layout pageTitle="Status"> </base-layout>
+  <base-layout pageTitle="Status">
+    <div class="ion-text-center">
+      <h3>Bomba</h3>
+    </div>
+
+    <ion-card>
+      <ion-card-header
+        class="ion-padding"
+        :class="[
+          bombaNoMomento == 1
+            ? 'ion-text-center card-success'
+            : 'ion-text-center card-alert',
+        ]"
+      >
+        <ion-card-title>
+          <span v-if="bombaNoMomento == 1"> Ligada</span>
+          <span v-if="bombaNoMomento == 0"> Desligada</span>
+        </ion-card-title>
+      </ion-card-header>
+    </ion-card>
+  </base-layout>
 </template>
 
 <script>
 import BaseLayout from "@/components/base/BaseLayout.vue";
+import { IonCard, IonCardHeader, IonCardTitle } from "@ionic/vue";
 
 export default {
   name: "Home",
   components: {
     BaseLayout,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+  },
+  data() {
+    return {
+      bombaNoMomento: 1,
+    };
   },
 };
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
+/* Especifc */
+/*LABEL DE CONSUMO */
 
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
+.card-success {
+  border-left: 5px solid #c3e6cb;
+  background-color: #c3e6cb;
 }
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
+.card-success ion-card-title {
+  color: #155724;
 }
 
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-
-  color: #8c8c8c;
-
-  margin: 0;
+.card-alert {
+  border-left: 5px solid #ffeeba;
+  background-color: #ffeeba;
 }
 
-#container a {
-  text-decoration: none;
+.card-alert ion-card-title {
+  color: #856404;
 }
 </style>
