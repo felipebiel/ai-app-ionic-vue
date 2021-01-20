0a1343c1a01e79133caf03acd4d53a85b15b0217
Felipe Biel <felipe.biel.souza@gmail.com>
2021-01-20 16:41:32 -0300

    new: controle da bomba d'agua no status (estático)

--- src/pages/Status.vue
+++ src/pages/Status.vue
@@ -39,6 +39,7 @@
       <h3>Contra Seco</h3>
     </div>
 
+    <!-- not Dry -->
     <ion-card>
       <ion-card-header
         class="ion-padding"
@@ -54,12 +55,55 @@
         </ion-card-title>
       </ion-card-header>
     </ion-card>
+
+    <!-- Pump control -->
+    <div class="ion-text-center">
+      <h3>Controle da bomba d'agua</h3>
+    </div>
+
+    <ion-list v-if="nivelNoMomento == 3" class="ion-padding">
+      <ion-card-header class="ion-text-center card-alert">
+        <ion-card-title>
+          <p>O reservatório está cheio, nenhuma ação é permitida</p>
+        </ion-card-title>
+      </ion-card-header>
+    </ion-list>
+
+    <ion-list v-if="contraSecoNoMomento == 0" class="ion-padding">
+      <ion-card-header class="ion-text-center card-alert">
+        <ion-card-title>
+          <p>Não tem água, nenhuma ação é permitida</p>
+        </ion-card-title>
+      </ion-card-header>
+    </ion-list>
+
+    <ion-list
+      v-if="bombaNoMomento == 1 && contraSecoNoMomento == 1"
+      class="ion-padding"
+    >
+      <ion-button color="warning" expand="block">Desligar</ion-button>
+    </ion-list>
+
+    <ion-list
+      v-if="
+        bombaNoMomento == 0 && nivelNoMomento < 3 && contraSecoNoMomento == 1
+      "
+      class="ion-padding"
+    >
+      <ion-button color="warning" expand="block">Ligar</ion-button>
+    </ion-list>
   </base-layout>
 </template>
 
 <script>
 import BaseLayout from "@/components/base/BaseLayout.vue";
-import { IonCard, IonCardHeader, IonCardTitle } from "@ionic/vue";
+import {
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonList,
+  IonButton,
+} from "@ionic/vue";
 
 export default {
   name: "Home",
@@ -68,14 +112,16 @@ export default {
     IonCard,
     IonCardHeader,
     IonCardTitle,
+    IonList,
+    IonButton,
   },
   data() {
     return {
-      bombaNoMomento: 1,
-      nivelNoMomento: 0,
+      bombaNoMomento: 0,
+      nivelNoMomento: 3,
       porcentagemReservatorio: 30,
       litrosReservatorio: 775,
-      contraSecoNoMomento: 1,
+      contraSecoNoMomento: 0,
     };
   },
   methods: {
