b77d2367ab3aae4b125adcc0bc85d3106960bf16
Felipe Biel <felipe.biel.souza@gmail.com>
2021-01-21 10:06:27 -0300

    new: Criação do componente de PumpControl

---
+++ src/components/status/PumpControl.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="ion-text-center">
+    <h3>Controle da bomba d'agua</h3>
+  </div>
+
+  <ion-list v-if="nivelNoMomento == 3" class="ion-padding">
+    <ion-card-header class="ion-text-center card-alert">
+      <ion-card-title>
+        <p>O reservatório está cheio, nenhuma ação é permitida</p>
+      </ion-card-title>
+    </ion-card-header>
+  </ion-list>
+
+  <ion-list v-if="contraSecoNoMomento == 0" class="ion-padding">
+    <ion-card-header class="ion-text-center card-alert">
+      <ion-card-title>
+        <p>Não tem água, nenhuma ação é permitida</p>
+      </ion-card-title>
+    </ion-card-header>
+  </ion-list>
+
+  <ion-list
+    v-if="bombaNoMomento == 1 && contraSecoNoMomento == 1"
+    class="ion-padding"
+  >
+    <ion-button color="warning" expand="block">Desligar</ion-button>
+  </ion-list>
+
+  <ion-list
+    v-if="bombaNoMomento == 0 && nivelNoMomento < 3 && contraSecoNoMomento == 1"
+    class="ion-padding"
+  >
+    <ion-button color="warning" expand="block">Ligar</ion-button>
+  </ion-list>
+</template>
+
+<script>
+import { IonList, IonCardHeader, IonCardTitle, IonButton } from "@ionic/vue";
+export default {
+  props: ["nivelNoMomento", "contraSecoNoMomento", "bombaNoMomento"],
+  components: {
+    IonList,
+    IonCardHeader,
+    IonCardTitle,
+    IonButton,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
