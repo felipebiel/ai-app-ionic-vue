2cfa20b61ede193548ba43aa477dc928cd9e0a8b
Felipe Biel <felipe.biel.souza@gmail.com>
2021-01-21 10:12:50 -0300

    new: Criação do componente de loadingPage

---
+++ src/components/base/LoadingPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="ion-text-center ion-margin">{{ loadingMessage }}</div>
+    <ion-progress-bar type="indeterminate"></ion-progress-bar>
+  </div>
+</template>
+
+<script>
+import { IonProgressBar } from "@ionic/vue";
+export default {
+  props: ["loadingMessage"],
+  components: {
+    IonProgressBar,
+  },
+};
+</script>
+
+<style lang="css" scoped>
+</style>
