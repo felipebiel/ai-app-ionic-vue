93160a2ff0f3565afd048dd568fd197b122308dd
Felipe Biel <felipe.biel.souza@gmail.com>
2021-01-21 10:02:04 -0300

    new: Criação do componente de NotDry

---
+++ src/components/status/NotDry.vue
@@ -0,0 +1,33 @@
+<template>
+  <ion-card>
+    <ion-card-header
+      class="ion-padding"
+      :class="[
+        contraSecoNoMomento == 1
+          ? 'ion-text-center card-success'
+          : 'ion-text-center card-alert',
+      ]"
+    >
+      <ion-card-title>
+        <span v-if="contraSecoNoMomento == 1"> Tem água</span>
+        <span v-if="contraSecoNoMomento == 0"> Sem água</span>
+      </ion-card-title>
+    </ion-card-header>
+  </ion-card>
+</template>
+
+<script>
+import { IonCard, IonCardHeader, IonCardTitle } from "@ionic/vue";
+
+export default {
+  props: ["contraSecoNoMomento"],
+  components: {
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+  },
+};
+</script>
+
+<style lang="css" scoped>
+</style>
