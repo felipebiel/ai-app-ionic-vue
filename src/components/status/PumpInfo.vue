00bb698ea0e30aa0b9d5e32de179fe00fbd7aa57
Felipe Biel <felipe.biel.souza@gmail.com>
2021-01-21 09:46:19 -0300

    new: Criação do componente de PumpInfo

---
+++ src/components/status/PumpInfo.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="ion-text-center">
+    <h3>Bomba</h3>
+  </div>
+
+  <ion-card>
+    <ion-card-header
+      class="ion-padding"
+      :class="[
+        bombaNoMomento == 1
+          ? 'ion-text-center card-success'
+          : 'ion-text-center card-alert',
+      ]"
+    >
+      <ion-card-title>
+        <span v-if="bombaNoMomento == 1"> Ligada</span>
+        <span v-if="bombaNoMomento == 0"> Desligada</span>
+      </ion-card-title>
+    </ion-card-header>
+  </ion-card>
+</template>
+
+<script>
+import { IonCard, IonCardTitle, IonCardHeader } from "@ionic/vue";
+export default {
+  props: ["bombaNoMomento"],
+  components: {
+    IonCard,
+    IonCardTitle,
+    IonCardHeader,
+  },
+};
+</script>
+
+<style lang="css" scoped>
+</style>
