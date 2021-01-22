e3ab41ac56836afa14624df70f0693e74b64fe96
Felipe Biel <felipe.biel.souza@gmail.com>
2021-01-22 11:37:07 -0300

    new: Colocando o sideMenu no App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <ion-app>
-    <ion-router-outlet />
+    <side-menu />
+    <ion-router-outlet id="main-content" />
   </ion-app>
 </template>
 
 <script>
 import { IonApp, IonRouterOutlet } from "@ionic/vue";
 import { defineComponent } from "vue";
+import SideMenu from "@/components/base/SideMenu";
 
 export default defineComponent({
   name: "App",
   components: {
     IonApp,
     IonRouterOutlet,
+    SideMenu,
   },
 });
 </script>
