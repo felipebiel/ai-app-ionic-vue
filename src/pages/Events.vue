ac9d09991ec7528cc00cc3f219c448417028ae6a
Felipe Biel <felipe.biel.souza@gmail.com>
2021-01-25 09:34:52 -0300

    new:: conteudo estático do eventos

--- src/pages/Events.vue
+++ src/pages/Events.vue
@@ -1,6 +1,45 @@
 <template>
   <base-layout pageTitle="Status">
-    <div class="fadeIn" v-if="!progresso"></div>
+    <div class="fadeIn" v-if="!progresso">
+      <ion-card v-for="evento in events" :key="evento.id">
+        <ion-card-header class="card-info">
+          <ion-card-title>
+            <ion-icon :icon="alertCircleOutline"></ion-icon> Informativo
+          </ion-card-title>
+          <p>
+            <b> <ion-icon name="list-box"></ion-icon> Nível: </b>
+            <span :class="['text-nivel' + evento.currentLevel]">{{
+              tratarNivel(evento.currentLevel)
+            }}</span>
+          </p>
+          <p>
+            <b>
+              <ion-icon name="speedometer"></ion-icon>
+              Bomba:
+            </b>
+            <span
+              :class="[evento.bombaOnOff == 1 ? 'text-success' : 'text-danger']"
+              >{{ evento.bombaOnOff == 1 ? "Ligada" : "Desligada" }}</span
+            >
+          </p>
+          <p>
+            <b> <ion-icon name="water"></ion-icon> Contra-Seco: </b>
+            <span
+              :class="[
+                evento.contraSecoOnOff == 1 ? 'text-success' : 'text-danger',
+              ]"
+              >{{
+                evento.contraSecoOnOff == 1 ? "Tem água" : "Não tem água"
+              }}</span
+            >
+          </p>
+          <ion-card-subtitle end>
+            <ion-icon ios="ios-calendar" md="md-calendar"></ion-icon>
+            {{ dateAgo(evento.dateHourLevelCurrent) }}
+          </ion-card-subtitle>
+        </ion-card-header>
+      </ion-card>
+    </div>
     <loading-page
       class="ion-padding fadeIn"
       v-else
@@ -10,6 +49,16 @@
 </template>
 
 <script>
+import {
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonIcon,
+  IonCardSubtitle,
+} from "@ionic/vue";
+
+import { alertCircleOutline } from "ionicons/icons";
+
 import BaseLayout from "@/components/base/BaseLayout.vue";
 import LoadingPage from "@/components/base/LoadingPage.vue";
 
@@ -18,10 +67,53 @@ export default {
   components: {
     BaseLayout,
     LoadingPage,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonIcon,
+    IonCardSubtitle,
   },
   data() {
     return {
       progresso: true,
+      alertCircleOutline,
+      events: [
+        {
+          id: 1,
+          bombaOnOff: 0,
+          currentLevel: 3,
+          contraSecoOnOff: 1,
+          dateHourLevelCurrent: Date.now(),
+        },
+        {
+          id: 2,
+          bombaOnOff: 1,
+          currentLevel: 3,
+          contraSecoOnOff: 1,
+          dateHourLevelCurrent: Date.now(),
+        },
+        {
+          id: 3,
+          bombaOnOff: 1,
+          currentLevel: 2,
+          contraSecoOnOff: 1,
+          dateHourLevelCurrent: Date.now(),
+        },
+        {
+          id: 4,
+          bombaOnOff: 1,
+          currentLevel: 1,
+          contraSecoOnOff: 1,
+          dateHourLevelCurrent: Date.now(),
+        },
+        {
+          id: 5,
+          bombaOnOff: 1,
+          currentLevel: 0,
+          contraSecoOnOff: 1,
+          dateHourLevelCurrent: Date.now(),
+        },
+      ],
     };
   },
   created() {
@@ -30,8 +122,145 @@ export default {
       this.progresso = false;
     }, 3000);
   },
+  methods: {
+    dateAgo(value) {
+      return value;
+    },
+    tratarNivel(value) {
+      switch (value) {
+        case 0:
+          value = "Vazio";
+          break;
+        case 1:
+          value = "Baixo";
+          break;
+        case 2:
+          value = "Médio";
+          break;
+
+        case 3:
+          value = "Cheio";
+          break;
+        default:
+          break;
+      }
+      return value;
+    },
+  },
 };
 </script>
 
 <style scoped>
+/*COR DOS CABEÇALHOS*/
+
+ion-toolbar {
+  --background: #174580;
+  color: white;
+}
+
+ion-tab-bar {
+  --background: #174580;
+  color: white;
+}
+
+/*COR DOS CARDS*/
+
+.card-success {
+  border-left: 5px solid #c3e6cb;
+  background-color: #c3e6cb;
+}
+
+.card-success ion-card-title {
+  color: #155724;
+}
+
+.card-success ion-card-subtitle {
+  color: #155724;
+}
+
+.card-danger {
+  border-left: 5px solid #f5c6cb;
+  background-color: #f5c6cb;
+}
+
+.card-danger ion-card-title {
+  color: #721c24;
+}
+
+.card-danger ion-card-subtitle {
+  color: #721c24;
+}
+
+.card-info {
+  border-left: 5px solid lightblue;
+  background-color: lightblue;
+}
+
+.card-info ion-card-title {
+  color: #0c5460;
+}
+
+.card-info p {
+  color: #0c5460;
+}
+
+.card-info ion-card-subtitle {
+  color: #0c5460;
+}
+
+.card-alert {
+  border-left: 5px solid #ffeeba;
+  background-color: #ffeeba;
+}
+
+.card-alert ion-card-title {
+  color: #856404;
+}
+
+.card-alert ion-card-subtitle {
+  color: #856404;
+}
+
+/* Custom Skeleton Line Height and Margin */
+.custom-skeleton ion-skeleton-text {
+  line-height: 13px;
+}
+
+.custom-skeleton ion-skeleton-text:last-child {
+  margin-bottom: 5px;
+}
+
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+}
+
+.fadeIn {
+  animation-name: fadeIn;
+  animation-duration: 1s;
+}
+
+.text-danger {
+  color: #871111;
+}
+.text-success {
+  color: #14950e;
+}
+.text-nivel0 {
+  color: #871111;
+}
+.text-nivel1 {
+  color: #b88814;
+}
+.text-nivel2 {
+  color: #829e23;
+}
+.text-nivel3 {
+  color: #14950e;
+}
 </style>
