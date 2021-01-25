099c691907936f5dfb059a02d316b6f7d6979914
Felipe Biel <felipe.biel.souza@gmail.com>
2021-01-25 10:48:35 -0300

    new: Simulando infinit scroll no eventos

--- src/components/events/EventsListItem.vue
+++ src/components/events/EventsListItem.vue
@@ -2,16 +2,50 @@
   <ion-card v-for="evento in events" :key="evento.id">
     <events-item :evento="evento"></events-item>
   </ion-card>
+  <ion-infinite-scroll
+    @ionInfinite="loadData()"
+    threshold="100px"
+    id="infinite-scroll"
+    :disabled="isDisabled"
+  >
+    <ion-infinite-scroll-content
+      loading-spinner="circular"
+      loading-text="Carregando mais eventos..."
+    >
+    </ion-infinite-scroll-content>
+  </ion-infinite-scroll>
 </template>
 
 <script>
 import EventsItem from "./EventsItem";
-import { IonCard } from "@ionic/vue";
+import {
+  IonCard,
+  IonInfiniteScroll,
+  IonInfiniteScrollContent,
+} from "@ionic/vue";
 export default {
   props: ["events"],
   components: {
     IonCard,
     EventsItem,
+    IonInfiniteScroll,
+    IonInfiniteScrollContent,
+  },
+  data() {
+    return {
+      isDisabled: false,
+    };
+  },
+  methods: {
+    loadData() {
+      console.log("HERE");
+      this.$store.dispatch("loadMoreEvens").then(() => {
+        this.isDisabled = !this.isDisabled;
+        setTimeout(() => {
+          this.isDisabled = !this.isDisabled;
+        }, 1000);
+      });
+    },
   },
 };
 </script>
