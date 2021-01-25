f41c6657be28420f6a262d3c89bee980f453d1c9
Felipe Biel <felipe.biel.souza@gmail.com>
2021-01-25 10:16:36 -0300

    new: Setando eventos no state de eventos no vuex de event

--- src/store/modules/event.js
+++ src/store/modules/event.js
@@ -4,13 +4,50 @@ export default {
     },
     mutations: {
         SET_EVENTS(state, data){
+            state.events = data;
         },
     },
     actions: {
         loadEvents(context) {
-            return new Promise((resolve, reject) => {
-                    resolve('ok');
-                    reject();
+            return new Promise((resolve) => {
+                context.commit('SET_EVENTS', [
+                    {
+                      id: 1,
+                      bombaOnOff: 0,
+                      currentLevel: 3,
+                      contraSecoOnOff: 1,
+                      dateHourLevelCurrent: Date.now(),
+                    },
+                    {
+                      id: 2,
+                      bombaOnOff: 1,
+                      currentLevel: 3,
+                      contraSecoOnOff: 1,
+                      dateHourLevelCurrent: Date.now() - 5000,
+                    },
+                    {
+                      id: 3,
+                      bombaOnOff: 1,
+                      currentLevel: 2,
+                      contraSecoOnOff: 1,
+                      dateHourLevelCurrent: Date.now() - 6000,
+                    },
+                    {
+                      id: 4,
+                      bombaOnOff: 1,
+                      currentLevel: 1,
+                      contraSecoOnOff: 1,
+                      dateHourLevelCurrent: Date.now() - 7000,
+                    },
+                    {
+                      id: 5,
+                      bombaOnOff: 1,
+                      currentLevel: 0,
+                      contraSecoOnOff: 1,
+                      dateHourLevelCurrent: Date.now() - 150000,
+                    },
+                  ])
+                  resolve();
             });
         }
     }
