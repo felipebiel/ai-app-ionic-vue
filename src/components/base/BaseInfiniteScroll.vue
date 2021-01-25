9be2a6d3521d5cf999c6e70640dafa182d5f19a7
Felipe Biel <felipe.biel.souza@gmail.com>
2021-01-25 10:56:59 -0300

    new: Componentizando infinite scholl no EventsListItem

---
+++ src/components/base/BaseInfiniteScroll.vue
@@ -0,0 +1,34 @@
+<template>
+  <ion-infinite-scroll
+    @ionInfinite="loadData($event)"
+    threshold="100px"
+    id="infinite-scroll"
+    :disabled="disabled"
+  >
+    <ion-infinite-scroll-content
+      :loading-spinner="spinner"
+      :loading-text="text"
+    >
+    </ion-infinite-scroll-content>
+  </ion-infinite-scroll>
+</template>
+
+<script>
+import { IonInfiniteScroll, IonInfiniteScrollContent } from "@ionic/vue";
+export default {
+  emits: ["loadData"],
+  props: ["spinner", "text", "disabled"],
+  components: {
+    IonInfiniteScroll,
+    IonInfiniteScrollContent,
+  },
+  methods: {
+    loadData(e) {
+      this.$emit("loadData", e);
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- src/components/events/EventsListItem.vue
+++ src/components/events/EventsListItem.vue
@@ -2,34 +2,27 @@
   <ion-card v-for="evento in events" :key="evento.id">
     <events-item :evento="evento"></events-item>
   </ion-card>
-  <ion-infinite-scroll
-    @ionInfinite="loadData()"
-    threshold="100px"
-    id="infinite-scroll"
+
+  <base-infinite-scroll
+    spinner="circular"
+    text="Carregando eventos ..."
     :disabled="isDisabled"
-  >
-    <ion-infinite-scroll-content
-      loading-spinner="circular"
-      loading-text="Carregando mais eventos..."
-    >
-    </ion-infinite-scroll-content>
-  </ion-infinite-scroll>
+    @loadData="loadData"
+  ></base-infinite-scroll>
 </template>
 
 <script>
 import EventsItem from "./EventsItem";
-import {
-  IonCard,
-  IonInfiniteScroll,
-  IonInfiniteScrollContent,
-} from "@ionic/vue";
+import BaseInfiniteScroll from "../base/BaseInfiniteScroll";
+
+import { IonCard } from "@ionic/vue";
+
 export default {
   props: ["events"],
   components: {
     IonCard,
     EventsItem,
-    IonInfiniteScroll,
-    IonInfiniteScrollContent,
+    BaseInfiniteScroll,
   },
   data() {
     return {
@@ -38,7 +31,6 @@ export default {
   },
   methods: {
     loadData() {
-      console.log("HERE");
       this.$store.dispatch("loadMoreEvens").then(() => {
         this.isDisabled = !this.isDisabled;
         setTimeout(() => {
