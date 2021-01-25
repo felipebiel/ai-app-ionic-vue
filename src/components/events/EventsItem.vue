c5b10e35dcad68b021f1aae713d954ac71f412e9
Felipe Biel <felipe.biel.souza@gmail.com>
2021-01-25 10:02:05 -0300

    fix: icone e tamanho da hora do evento

--- src/components/events/EventsItem.vue
+++ src/components/events/EventsItem.vue
@@ -1,38 +1,38 @@
 <template>
-    <ion-card-header class="card-info">
-      <ion-card-title>
-        <ion-icon :icon="alertCircleOutline"></ion-icon> Informativo
-      </ion-card-title>
-      <p>
-        <b> <ion-icon name="list-box"></ion-icon> Nível: </b>
-        <span :class="['text-nivel' + evento.currentLevel]">{{
-          $utils.tratarNivel(evento.currentLevel)
-        }}</span>
-      </p>
-      <p>
-        <b>
-          <ion-icon name="speedometer"></ion-icon>
-          Bomba:
-        </b>
-        <span
-          :class="[evento.bombaOnOff == 1 ? 'text-success' : 'text-danger']"
-          >{{ evento.bombaOnOff == 1 ? "Ligada" : "Desligada" }}</span
-        >
-      </p>
-      <p>
-        <b> <ion-icon name="water"></ion-icon> Contra-Seco: </b>
-        <span
-          :class="[
-            evento.contraSecoOnOff == 1 ? 'text-success' : 'text-danger',
-          ]"
-          >{{ evento.contraSecoOnOff == 1 ? "Tem água" : "Não tem água" }}</span
-        >
-      </p>
-      <ion-card-subtitle end>
-        <ion-icon ios="ios-calendar" md="md-calendar"></ion-icon>
-        {{ $utils.dateAgo(evento.dateHourLevelCurrent) }}
-      </ion-card-subtitle>
-    </ion-card-header>
+  <ion-card-header class="card-info">
+    <ion-card-title>
+      <ion-icon :icon="alertCircleOutline"></ion-icon> Informativo
+    </ion-card-title>
+    <p>
+      <b> <ion-icon name="list-box"></ion-icon> Nível: </b>
+      <span :class="['text-nivel' + evento.currentLevel]">{{
+        $utils.tratarNivel(evento.currentLevel)
+      }}</span>
+    </p>
+    <p>
+      <b>
+        <ion-icon name="speedometer"></ion-icon>
+        Bomba:
+      </b>
+      <span
+        :class="[evento.bombaOnOff == 1 ? 'text-success' : 'text-danger']"
+        >{{ evento.bombaOnOff == 1 ? "Ligada" : "Desligada" }}</span
+      >
+    </p>
+    <p>
+      <b> <ion-icon name="water"></ion-icon> Contra-Seco: </b>
+      <span
+        :class="[evento.contraSecoOnOff == 1 ? 'text-success' : 'text-danger']"
+        >{{ evento.contraSecoOnOff == 1 ? "Tem água" : "Não tem água" }}</span
+      >
+    </p>
+    <ion-card-subtitle end>
+      <small
+        ><ion-icon :icon="timeOutline"></ion-icon>
+        {{ $utils.dateAgo(evento.dateHourLevelCurrent) }}</small
+      >
+    </ion-card-subtitle>
+  </ion-card-header>
 </template>
 
 <script>
@@ -43,7 +43,7 @@ import {
   IonCardSubtitle,
 } from "@ionic/vue";
 
-import { alertCircleOutline } from "ionicons/icons";
+import { alertCircleOutline, timeOutline } from "ionicons/icons";
 
 export default {
   name: "EventsItem",
@@ -57,6 +57,7 @@ export default {
   data() {
     return {
       alertCircleOutline,
+      timeOutline,
     };
   },
 };
