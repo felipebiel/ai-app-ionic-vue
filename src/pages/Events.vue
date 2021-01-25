0ee01e1c60b7b379bf27f35b52a74dee98583ee2
Felipe Biel <felipe.biel.souza@gmail.com>
2021-01-25 10:16:55 -0300

    Usando o vuex na lista de eventos

--- src/pages/Events.vue
+++ src/pages/Events.vue
@@ -26,50 +26,20 @@ export default {
   data() {
     return {
       progresso: true,
-      events: [
-        {
-          id: 1,
-          bombaOnOff: 0,
-          currentLevel: 3,
-          contraSecoOnOff: 1,
-          dateHourLevelCurrent: Date.now(),
-        },
-        {
-          id: 2,
-          bombaOnOff: 1,
-          currentLevel: 3,
-          contraSecoOnOff: 1,
-          dateHourLevelCurrent: Date.now() - 5000,
-        },
-        {
-          id: 3,
-          bombaOnOff: 1,
-          currentLevel: 2,
-          contraSecoOnOff: 1,
-          dateHourLevelCurrent: Date.now() - 6000,
-        },
-        {
-          id: 4,
-          bombaOnOff: 1,
-          currentLevel: 1,
-          contraSecoOnOff: 1,
-          dateHourLevelCurrent: Date.now() - 7000,
-        },
-        {
-          id: 5,
-          bombaOnOff: 1,
-          currentLevel: 0,
-          contraSecoOnOff: 1,
-          dateHourLevelCurrent: Date.now() - 150000,
-        },
-      ],
     };
   },
   created() {
     //simulando carregamento
     setTimeout(() => {
-      this.progresso = false;
+      this.$store.dispatch("loadEvents").then(() => {
+        this.progresso = false;
+      });
     }, 3000);
+  },
+  computed: {
+    events() {
+      return this.$store.state.event.events;
+    },
   },
 };
 </script>
