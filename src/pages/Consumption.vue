22ccce721c37621f1a2c9c8cebdc8fc536e92c3a
Felipe Biel <felipe.biel.souza@gmail.com>
2021-01-25 16:07:16 -0300

    test: testando gráfico no consumo

--- src/pages/Consumption.vue
+++ src/pages/Consumption.vue
@@ -1,9 +1,11 @@
 <template>
   <base-layout pageTitle="Status">
-    <div class="fadeIn" v-if="!progresso"></div>
+    <div class="fadeIn" v-show="!progresso">
+      <canvas width="400" ref="myChart" height="400"></canvas>
+    </div>
     <loading-page
       class="ion-padding fadeIn"
-      v-else
+      v-show="progresso"
       loadingMessage="Carregando"
     ></loading-page>
   </base-layout>
@@ -12,6 +14,7 @@
 <script>
 import BaseLayout from "@/components/base/BaseLayout.vue";
 import LoadingPage from "@/components/base/LoadingPage.vue";
+import Chart from "chart.js";
 
 export default {
   name: "Events",
@@ -27,6 +30,48 @@ export default {
   created() {
     //simulando carregamento
     setTimeout(() => {
+      const ctx = this.$refs.myChart;
+      new Chart(ctx, {
+        type: "bar",
+        data: {
+          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+          datasets: [
+            {
+              label: "# of Votes",
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(153, 102, 255, 0.2)",
+                "rgba(255, 159, 64, 0.2)",
+              ],
+              borderColor: [
+                "rgba(255, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(153, 102, 255, 1)",
+                "rgba(255, 159, 64, 1)",
+              ],
+              borderWidth: 1,
+            },
+          ],
+        },
+        options: {
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                },
+              },
+            ],
+          },
+        },
+      });
+
       this.progresso = false;
     }, 3000);
   },
