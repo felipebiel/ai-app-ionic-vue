0b318bebe6c5ca8e68cf2a33ee3c633b893eb447
Felipe Biel <felipe.biel.souza@gmail.com>
2021-01-26 10:53:22 -0300

    new: Formulário de Meus Dados completo

--- src/pages/Me.vue
+++ src/pages/Me.vue
@@ -1,16 +1,107 @@
 <template>
   <base-layout pageTitle="Meus Dados" pageDefaultBackLink="/tabs">
-    <div class="fadeIn ion-padding"></div>
+    <div class="fadeIn ion-padding">
+      <ion-item>
+        <ion-label position="stacked">Usuário</ion-label>
+        <ion-input v-model="form.username" disabled></ion-input>
+      </ion-item>
+
+      <ion-item>
+        <ion-label position="stacked">E-mail</ion-label>
+        <ion-input v-model="form.email" disabled></ion-input>
+      </ion-item>
+
+      <ion-item>
+        <ion-label position="stacked">Primeiro Nome</ion-label>
+        <ion-input v-model="form.firstname"></ion-input>
+      </ion-item>
+
+      <ion-text color="danger" v-if="firstname_request">
+        <p text-center class="ion-no-margin">{{ firstname_message }}</p>
+      </ion-text>
+
+      <ion-item>
+        <ion-label position="stacked">Sobrenome</ion-label>
+        <ion-input v-model="form.lastname"></ion-input>
+      </ion-item>
+
+      <ion-text color="danger" v-if="lastname_request">
+        <p text-center class="ion-no-margin">{{ lastname_message }}</p>
+      </ion-text>
+
+      <ion-item>
+        <ion-label position="stacked">CPF</ion-label>
+        <ion-input v-model="form.cpf" disabled></ion-input>
+      </ion-item>
+
+      <ion-list>
+        <ion-button margin-top expand="block" @click="alterarUser()"
+          >Alterar</ion-button
+        >
+      </ion-list>
+
+      <ion-list>
+        <ion-button margin-top expand="block" @click="confirmRedefinirSenha()"
+          >Alterar a Senha</ion-button
+        >
+      </ion-list>
+    </div>
   </base-layout>
 </template>
 
 <script>
+import {
+  IonItem,
+  IonButton,
+  IonLabel,
+  IonInput,
+  IonText,
+  IonList,
+  toastController,
+} from "@ionic/vue";
 import BaseLayout from "@/components/base/BaseLayout.vue";
 
 export default {
   name: "Me",
   components: {
     BaseLayout,
+    IonItem,
+    IonButton,
+    IonLabel,
+    IonInput,
+    IonText,
+    IonList,
+  },
+  data() {
+    return {
+      form: {
+        username: "FelipeBiel",
+        email: "felipe@email.com",
+        firstname: "Felipe",
+        lastname: "Biel",
+        cpf: "000000000000",
+      },
+      firstname_request: false,
+      lastname_request: false,
+      firstname_message: "",
+      lastname_message: "",
+    };
+  },
+  methods: {
+    alterarUser() {
+      this.openToast();
+    },
+    confirmRedefinirSenha() {
+      this.openToast();
+    },
+    async openToast() {
+      const toast = await toastController.create({
+        message: "Salvo com sucesso.",
+        duration: 2000,
+        color: "success",
+      });
+      return toast.present();
+    },
   },
 };
 </script>
