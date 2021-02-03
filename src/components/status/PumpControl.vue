dd29c345eff8380aacdcd94b952887ed6fae29cf
Felipe Biel <felipe.biel.souza@gmail.com>
2021-02-03 09:24:56 -0300

    change: mudança visual no componente de PumpControl

--- src/components/status/PumpControl.vue
+++ src/components/status/PumpControl.vue
@@ -1,45 +1,62 @@
 <template>
-  <div class="ion-text-center">
-    <h3>Controle da bomba d'agua</h3>
-  </div>
-
-  <ion-list v-if="nivelNoMomento == 3" class="ion-padding">
-    <ion-card-header class="ion-text-center card-alert">
-      <ion-card-title>
-        <p>O reservatório está cheio, nenhuma ação é permitida</p>
-      </ion-card-title>
-    </ion-card-header>
-  </ion-list>
-
-  <ion-list v-if="contraSecoNoMomento == 0" class="ion-padding">
-    <ion-card-header class="ion-text-center card-alert">
-      <ion-card-title>
-        <p>Não tem água, nenhuma ação é permitida</p>
-      </ion-card-title>
-    </ion-card-header>
-  </ion-list>
-
-  <ion-list
-    v-if="bombaNoMomento == 1 && contraSecoNoMomento == 1"
-    class="ion-padding"
+  <ion-card
+    class="ion-text-center elevation-2x custom-border-radius-card margin-top-2x"
   >
-    <ion-button color="warning" expand="block">Desligar</ion-button>
-  </ion-list>
+    <div class="ion-text-center">
+      <h3>Controle da bomba d'agua</h3>
+    </div>
 
-  <ion-list
-    v-if="bombaNoMomento == 0 && nivelNoMomento < 3 && contraSecoNoMomento == 1"
-    class="ion-padding"
-  >
-    <ion-button color="warning" expand="block">Ligar</ion-button>
-  </ion-list>
+    <ion-list v-if="nivelNoMomento == 3" class="ion-padding">
+      <ion-card-header
+        class="ion-text-center card-alert custom-border-radius-card"
+      >
+        <ion-card-title>
+          <p>O reservatório está cheio, nenhuma ação é permitida</p>
+        </ion-card-title>
+      </ion-card-header>
+    </ion-list>
+
+    <ion-list v-if="contraSecoNoMomento == 0" class="ion-padding">
+      <ion-card-header
+        class="ion-text-center card-alert custom-border-radius-card"
+      >
+        <ion-card-title>
+          <p>Não tem água, nenhuma ação é permitida</p>
+        </ion-card-title>
+      </ion-card-header>
+    </ion-list>
+
+    <ion-list
+      v-if="bombaNoMomento == 1 && contraSecoNoMomento == 1"
+      class="ion-padding"
+    >
+      <ion-button color="warning" expand="block">Desligar</ion-button>
+    </ion-list>
+
+    <ion-list
+      v-if="
+        bombaNoMomento == 0 && nivelNoMomento < 3 && contraSecoNoMomento == 1
+      "
+      class="ion-padding"
+    >
+      <ion-button color="warning" expand="block">Ligar</ion-button>
+    </ion-list>
+  </ion-card>
 </template>
 
 <script>
-import { IonList, IonCardHeader, IonCardTitle, IonButton } from "@ionic/vue";
+import {
+  IonList,
+  IonCardHeader,
+  IonCardTitle,
+  IonButton,
+  IonCard,
+} from "@ionic/vue";
 export default {
   props: ["nivelNoMomento", "contraSecoNoMomento", "bombaNoMomento"],
   components: {
     IonList,
+    IonCard,
     IonCardHeader,
     IonCardTitle,
     IonButton,
