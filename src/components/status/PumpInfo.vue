9f723825a505c300aaa2e025c8fd128044072978
Felipe Biel <felipe.biel.souza@gmail.com>
2021-02-03 09:23:17 -0300

    change: mudança visual no componente de PumpInfo

--- src/components/status/PumpInfo.vue
+++ src/components/status/PumpInfo.vue
@@ -1,22 +1,20 @@
 <template>
-  <div class="ion-text-center">
-    <h3>Bomba</h3>
-  </div>
-
-  <ion-card>
-    <ion-card-header
-      class="ion-padding"
-      :class="[
-        bombaNoMomento == 1
-          ? 'ion-text-center card-success'
-          : 'ion-text-center card-alert',
-      ]"
-    >
-      <ion-card-title>
-        <span v-if="bombaNoMomento == 1"> Ligada</span>
-        <span v-if="bombaNoMomento == 0"> Desligada</span>
-      </ion-card-title>
+  <ion-card
+    class="ion-text-center elevation-2x custom-border-radius-card"
+    :class="[
+      bombaNoMomento == 1
+        ? 'ion-text-center card-success'
+        : 'ion-text-center card-alert',
+    ]"
+  >
+    <ion-card-header class="ion-text-center">
+      <h3>Bomba</h3>
     </ion-card-header>
+
+    <ion-card-title class="ion-padding-bottom">
+      <span v-if="bombaNoMomento == 1"> Ligada</span>
+      <span v-if="bombaNoMomento == 0"> Desligada</span>
+    </ion-card-title>
   </ion-card>
 </template>
 
