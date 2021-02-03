b6a60a613e31775d32639ede41e24684c0a89808
Felipe Biel <felipe.biel.souza@gmail.com>
2021-02-03 09:25:22 -0300

    change: mudança visual na view de Status

--- src/pages/Status.vue
+++ src/pages/Status.vue
@@ -28,6 +28,12 @@
       v-else
       loadingMessage="Carregando"
     ></loading-page>
+
+    <div class="cicle__top"></div>
+
+    <div class="cicle__middle"></div>
+
+    <div class="cicle__bottom"></div>
   </base-layout>
 </template>
 
@@ -53,9 +59,9 @@ export default {
     return {
       bombaNoMomento: 1,
       nivelNoMomento: 2,
-      porcentagemReservatorio: 30,
+      porcentagemReservatorio: 70,
       litrosReservatorio: 775,
-      contraSecoNoMomento: 0,
+      contraSecoNoMomento: 1,
       progresso: true,
     };
   },
@@ -69,4 +75,51 @@ export default {
 </script>
 
 <style scoped>
+.cicle__top {
+  height: 120px;
+  position: fixed;
+  top: -50px;
+  right: -20px;
+  width: 120px;
+  border-radius: 100%;
+  background: linear-gradient(
+    228deg,
+    rgba(2, 0, 36, 0.31556372549019607) 0%,
+    rgba(9, 9, 121, 0.365983893557423) 35%,
+    rgba(0, 212, 255, 0.44441526610644255) 100%
+  );
+  z-index: -2;
+}
+
+.cicle__middle {
+  height: 300px;
+  position: fixed;
+  top: 100px;
+  left: -170px;
+  width: 300px;
+  border-radius: 100%;
+  background: linear-gradient(
+    228deg,
+    rgba(2, 0, 36, 0.31556372549019607) 0%,
+    rgba(9, 9, 121, 0.365983893557423) 35%,
+    rgba(0, 212, 255, 0.44441526610644255) 100%
+  );
+  z-index: -2;
+}
+
+.cicle__bottom {
+  height: 300px;
+  position: fixed;
+  bottom: -150px;
+  right: -170px;
+  width: 300px;
+  border-radius: 100%;
+  background: linear-gradient(
+    228deg,
+    rgba(2, 0, 36, 0.31556372549019607) 0%,
+    rgba(9, 9, 121, 0.365983893557423) 35%,
+    rgba(0, 212, 255, 0.44441526610644255) 100%
+  );
+  z-index: -2;
+}
 </style>
