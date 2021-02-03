b865cfc6428c51be0eff8ca70bad108b57bfba83
Felipe Biel <felipe.biel.souza@gmail.com>
2021-02-03 10:08:02 -0300

    new: Componentizando o ConsuptionMonth

---
+++ src/components/consuption/ConsuptionMonth.vue
@@ -0,0 +1,77 @@
+<template>
+  <ion-card class="elevation-2x margin-top-2x custom-border-radius-card">
+    <ion-card-header>
+      <ion-card-title>Consumo nos ultimos 30 dias</ion-card-title>
+    </ion-card-header>
+    <ion-card-content>
+      <canvas ref="myChartMonth" height="300" class="ion-padding"></canvas>
+    </ion-card-content>
+  </ion-card>
+</template>
+
+<script>
+import {
+  IonCard,
+  IonCardTitle,
+  IonCardContent,
+  IonCardHeader,
+} from "@ionic/vue";
+import Chart from "chart.js";
+
+export default {
+  name: "ConsumptionMonth",
+  components: {
+    IonCardTitle,
+    IonCardHeader,
+    IonCardContent,
+    IonCard,
+  },
+  data() {
+    return {
+      days: [1, 2, 3, 4, 5, 6],
+      litrosDays: [150, 400, 500, 450, 45, 650],
+    };
+  },
+  created() {
+    //simulando carregamento
+    setTimeout(() => {
+      this.generateChart(this.days, this.litrosDays);
+    }, 3000);
+  },
+  methods: {
+    generateChart(labelsData, datasetsData) {
+      const ctx = this.$refs.myChartMonth;
+      new Chart(ctx, {
+        type: "line",
+        data: {
+          labels: labelsData,
+          datasets: [
+            {
+              label: "Litros por dia",
+              data: datasetsData,
+              backgroundColor: "rgba(23, 69, 128, 0.6)",
+              borderColor: "rgb(23, 69, 128)",
+              borderWidth: 1,
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                },
+              },
+            ],
+          },
+        },
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
