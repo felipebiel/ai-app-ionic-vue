8d39f6f854d927eb7a9344f09560043068835e9c
Felipe Biel <felipe.biel.souza@gmail.com>
2021-02-04 09:15:15 -0300

    change: Mudança visual no Settings

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -1,72 +1,81 @@
 <template>
   <base-layout pageTitle="Configuração" pageDefaultBackLink="/tabs">
     <div class="fadeIn ion-padding">
-      <ion-item>
-        <ion-label>Nível de acionamento</ion-label>
-        <ion-select
-          @ionChange="mudarNivel($event)"
-          interface="action-sheet"
-          :value="nivel"
-        >
-          <ion-select-option value="0">Vazio</ion-select-option>
-          <ion-select-option value="1">Baixo</ion-select-option>
-          <ion-select-option value="2">Médio</ion-select-option>
-        </ion-select>
-      </ion-item>
-
-      <ion-item>
-        <ion-label position="stacked"
-          >Quantidade do reservatório (litros):</ion-label
-        >
-        <ion-input v-model="reservoirCapacity" type="number"></ion-input>
-      </ion-item>
-
-      <ion-item>
-        <ion-label>Modo</ion-label>
-        <ion-select
-          @ionChange="mudarModo($event)"
-          interface="action-sheet"
-          :value="modo"
-        >
-          <ion-select-option value="1">Automatico</ion-select-option>
-          <ion-select-option value="0">Manual</ion-select-option>
-        </ion-select>
-      </ion-item>
-
-      <ion-text v-if="automatico">
-        <p>
-          <b>Modo automático:</b> A rotina do dispositivo é ativada a qualquer
-          hora do dia, respeitando sempre o nível de acionamento.
-        </p>
-      </ion-text>
-
-      <ion-text v-if="!automatico">
-        <p>
-          <b>Modo manual:</b> A rotina do dispositivo é ativada em um range de
-          horário especifico, respeitando sempre o nível de acionamento.
-        </p>
-      </ion-text>
-
-      <ion-item v-if="!automatico">
-        <ion-label>Hora de inicio</ion-label>
-        <ion-datetime displayFormat="HH:mm"></ion-datetime>
-      </ion-item>
-
-      <ion-item v-if="!automatico">
-        <ion-label>Hora de termino</ion-label>
-        <ion-datetime displayFormat="HH:mm"></ion-datetime>
-      </ion-item>
-
-      <ion-button expand="block" class="ion-margin-top" @click="updateDevice()"
-        >Salvar</ion-button
+      <ion-card
+        class="elevation-2x margin-top-2x custom-border-radius-card ion-padding"
       >
+        <ion-item>
+          <ion-label>Nível de acionamento</ion-label>
+          <ion-select
+            @ionChange="mudarNivel($event)"
+            interface="action-sheet"
+            :value="nivel"
+          >
+            <ion-select-option value="0">Vazio</ion-select-option>
+            <ion-select-option value="1">Baixo</ion-select-option>
+            <ion-select-option value="2">Médio</ion-select-option>
+          </ion-select>
+        </ion-item>
+
+        <ion-item>
+          <ion-label position="stacked"
+            >Quantidade do reservatório (litros):</ion-label
+          >
+          <ion-input v-model="reservoirCapacity" type="number"></ion-input>
+        </ion-item>
+
+        <ion-item>
+          <ion-label>Modo</ion-label>
+          <ion-select
+            @ionChange="mudarModo($event)"
+            interface="action-sheet"
+            :value="modo"
+          >
+            <ion-select-option value="1">Automatico</ion-select-option>
+            <ion-select-option value="0">Manual</ion-select-option>
+          </ion-select>
+        </ion-item>
+
+        <ion-text v-if="automatico">
+          <p>
+            <b>Modo automático:</b> A rotina do dispositivo é ativada a qualquer
+            hora do dia, respeitando sempre o nível de acionamento.
+          </p>
+        </ion-text>
+
+        <ion-text v-if="!automatico">
+          <p>
+            <b>Modo manual:</b> A rotina do dispositivo é ativada em um range de
+            horário especifico, respeitando sempre o nível de acionamento.
+          </p>
+        </ion-text>
+
+        <ion-item v-if="!automatico">
+          <ion-label>Hora de inicio</ion-label>
+          <ion-datetime displayFormat="HH:mm"></ion-datetime>
+        </ion-item>
+
+        <ion-item v-if="!automatico">
+          <ion-label>Hora de termino</ion-label>
+          <ion-datetime displayFormat="HH:mm"></ion-datetime>
+        </ion-item>
+
+        <ion-button
+          expand="block"
+          class="ion-margin-top"
+          @click="updateDevice()"
+          >Salvar</ion-button
+        >
+      </ion-card>
     </div>
+
     <cicles-on-screen></cicles-on-screen>
   </base-layout>
 </template>
 
 <script>
 import {
+  IonCard,
   IonItem,
   IonLabel,
   IonDatetime,
@@ -93,6 +102,7 @@ export default {
     IonButton,
     IonSelect,
     IonSelectOption,
+    IonCard,
     CiclesOnScreen,
   },
   data() {
